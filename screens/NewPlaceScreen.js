3da3ecd506eea07a61987c013041d174fbf36c17
Isaac Medina <isaac@Isaacs-Air.hsd1.il.comcast.net>
2020-07-28 12:54:24 -0500

    basic setup

---
+++ screens/NewPlaceScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const NewPlaceScreen = (props) => {
+  return (
+    <View>
+      <Text>NewPlaceScreen.js</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default NewPlaceScreen;
